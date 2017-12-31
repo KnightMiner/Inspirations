170777dce8ee24f768bef412a1493c96e72a5ec1
KnightMiner <knightminer4@gmail.com>
2017-12-30 18:32:24 -0600

    Remove animation on anvil arrow, its not really time based

--- src/main/java/knightminer/inspirations/plugins/jei/smashing/SmashingRecipeCategory.java
+++ src/main/java/knightminer/inspirations/plugins/jei/smashing/SmashingRecipeCategory.java
@@ -9,8 +9,6 @@ import javax.annotation.Nonnull;
 
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
-import mezz.jei.api.gui.IDrawableAnimated;
-import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
@@ -22,13 +20,12 @@ public class SmashingRecipeCategory implements IRecipeCategory<SmashingRecipeWra
 	public static final ResourceLocation BACKGROUND_LOC = Util.getResource("textures/gui/jei/anvil_smashing.png");
 
 	private final IDrawable background;
-	private final IDrawableAnimated arrow;
+	private final IDrawable arrow;
 
 	public SmashingRecipeCategory(IGuiHelper guiHelper) {
 		background = guiHelper.createDrawable(BACKGROUND_LOC, 0, 0, 160, 33, 0, 4, 0, 0);
 
-		IDrawableStatic arrowDrawable = guiHelper.createDrawable(BACKGROUND_LOC, 160, 0, 24, 17);
-		arrow = guiHelper.createAnimatedDrawable(arrowDrawable, 200, IDrawableAnimated.StartDirection.LEFT, false);
+		arrow = guiHelper.createDrawable(BACKGROUND_LOC, 160, 0, 24, 17);
 	}
 
 	@Nonnull
