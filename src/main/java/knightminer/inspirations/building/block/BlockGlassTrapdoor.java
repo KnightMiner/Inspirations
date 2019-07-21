e86321d2bb17d0d41e211dfc97ef411b7b92c1ca
TeamSpen210 <spencerb21@live.com>
2019-07-21 18:11:44 +1000

    Rewrite Glass Trapdoor for 1.14

--- src/main/java/knightminer/inspirations/building/block/BlockGlassTrapdoor.java
+++ src/main/java/knightminer/inspirations/building/block/BlockGlassTrapdoor.java
@@ -2,30 +2,18 @@ package knightminer.inspirations.building.block;
 
 import java.util.Random;
 
-import net.minecraft.block.BlockTrapDoor;
+import net.minecraft.block.Block;
+import net.minecraft.block.TrapDoorBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
-public class BlockGlassTrapdoor extends BlockTrapDoor {
+public class BlockGlassTrapdoor extends TrapDoorBlock {
 
 	public BlockGlassTrapdoor() {
-		super(Material.GLASS);
-		this.setHardness(0.3F);
-		this.setSoundType(SoundType.GLASS);
-		this.disableStats();
+		super(Block.Properties.create(Material.GLASS)
+			.hardnessAndResistance(0.3F)
+			.sound(SoundType.GLASS)
+		);
+		// this.disableStats();
 	}
-
-	/**
-	 * Returns the quantity of items to drop on block destruction.
-	 */
-	@Override
-	public int quantityDropped(Random random) {
-		return 0;
-	}
-
-	@Override
-	protected boolean canSilkHarvest() {
-		return true;
-	}
-
 }
