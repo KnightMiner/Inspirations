139fb5d5c38d59008cf79101bcc444cec6b70181
TeamSpen210 <spencerb21@live.com>
2019-07-23 17:20:37 +1000

    Update ClientUtil for 1.14

--- src/main/java/knightminer/inspirations/library/client/ClientUtil.java
+++ src/main/java/knightminer/inspirations/library/client/ClientUtil.java
@@ -1,30 +1,30 @@
 package knightminer.inspirations.library.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import knightminer.inspirations.Inspirations;
 import knightminer.inspirations.library.InspirationsRegistry;
-import knightminer.inspirations.library.ItemMetaKey;
 import knightminer.inspirations.library.util.TextureBlockUtil;
 import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.MissingTextureSprite;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.resources.IResourceManager;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.property.IExtendedBlockState;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.world.IEnviromentBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.model.data.EmptyModelData;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.lwjgl.opengl.GL11;
 import slimeknights.mantle.client.ModelHelper;
@@ -36,21 +36,21 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-@SideOnly(Side.CLIENT)
+@OnlyIn(Dist.CLIENT)
 public final class ClientUtil {
 	public static final String TAG_TEXTURE_PATH = "texture_path";
-	private static final Minecraft mc = Minecraft.getMinecraft();
+	private static final Minecraft mc = Minecraft.getInstance();
 	private ClientUtil() {}
 
-	private static Map<ItemMetaKey, Integer> colorCache = new HashMap<>();
+	private static Map<Item, Integer> colorCache = new HashMap<>();
 
 	/**
-	 * Gets the color for an ItemStack based on its item and metadata
+	 * Gets the color for an ItemStack
 	 * @param stack  Input stack
 	 * @return  Color for the stack
 	 */
 	public static int getStackColor(ItemStack stack) {
-		return colorCache.computeIfAbsent(new ItemMetaKey(stack), ClientUtil::getStackColor);
+		return colorCache.computeIfAbsent(stack.getItem(), ClientUtil::getStackColor);
 	}
 
 	/**
@@ -59,30 +59,32 @@ public final class ClientUtil {
 	 * @return  Color for the item meta combination
 	 * @author InsomniaKitten
 	 */
-	private static Integer getStackColor(ItemMetaKey key) {
-		IBakedModel model = mc.getRenderItem().getItemModelWithOverrides(key.makeItemStack(), null, null);
+	private static Integer getStackColor(Item key) {
+		IBakedModel model = mc.getItemRenderer().getItemModelWithOverrides(new ItemStack(key), null, null);
 		if(model == null) {
 			return -1;
 		}
-		TextureAtlasSprite sprite = model.getParticleTexture();
+		TextureAtlasSprite sprite = model.getParticleTexture(EmptyModelData.INSTANCE);
 		if(sprite == null) {
 			return -1;
 		}
-		int[] pixels = sprite.getFrameTextureData(0)[0];
 		float r = 0, g = 0, b = 0, count = 0;
 		float[] hsb = new float[3];
-		for (int argb : pixels) {
-			int ca = argb >> 24 & 0xFF;
-			int cr = argb >> 16 & 0xFF;
-			int cg = argb >> 8 & 0xFF;
-			int cb = argb & 0xFF;
-			if (ca > 0x7F && NumberUtils.max(cr, cg, cb) > 0x1F) {
-				Color.RGBtoHSB(ca, cr, cg, hsb);
-				float weight = hsb[1];
-				r += cr * weight;
-				g += cg * weight;
-				b += cb * weight;
-				count += weight;
+		for (int x = 0; x < sprite.getWidth(); x++) {
+			for (int y = 0; y < sprite.getHeight(); y++) {
+				int argb = sprite.getPixelRGBA(0, x, y);
+				int ca = argb >> 24 & 0xFF;
+				int cr = argb >> 16 & 0xFF;
+				int cg = argb >> 8 & 0xFF;
+				int cb = argb & 0xFF;
+				if (ca > 0x7F && NumberUtils.max(cr, cg, cb) > 0x1F) {
+					Color.RGBtoHSB(ca, cr, cg, hsb);
+					float weight = hsb[1];
+					r += cr * weight;
+					g += cg * weight;
+					b += cb * weight;
+					count += weight;
+				}
 			}
 		}
 		if (count > 0) {
@@ -107,13 +109,13 @@ public final class ClientUtil {
 	 * @return
 	 */
 	public static TextureAtlasSprite getSprite(ResourceLocation location) {
-		TextureMap textureMapBlocks = mc.getTextureMapBlocks();
+		AtlasTexture textureMapBlocks = mc.getTextureMap();
 		TextureAtlasSprite sprite = null;
 		if(location != null) {
-			sprite = textureMapBlocks.getTextureExtry(location.toString());
+			sprite = textureMapBlocks.getSprite(location);
 		}
 		if (sprite == null) {
-			sprite = textureMapBlocks.getMissingSprite();
+			sprite = textureMapBlocks.getSprite(MissingTextureSprite.getLocation());
 		}
 		return sprite;
 	}
@@ -145,32 +147,14 @@ public final class ClientUtil {
 		String texture = te.getTileData().getString(TAG_TEXTURE_PATH);
 		if(texture.isEmpty()) {
 			// load it from saved block
-			ItemStack stack = new ItemStack(te.getTileData().getCompoundTag(TextureBlockUtil.TAG_TEXTURE));
+			ItemStack stack = ItemStack.read(te.getTileData().getCompound(TextureBlockUtil.TAG_TEXTURE));
 			if(!stack.isEmpty()) {
 				Block block = Block.getBlockFromItem(stack.getItem());
-				texture = ModelHelper.getTextureFromBlock(block, stack.getItem().getMetadata(stack)).getIconName();
-				te.getTileData().setString(TAG_TEXTURE_PATH, texture);
+				texture = ModelHelper.getTextureFromBlockstate(block.getDefaultState()).getName().toString();
+				te.getTileData().putString(TAG_TEXTURE_PATH, texture);
 			}
 		}
 		return texture;
-	}
-
-	/**
-	 * Writes the default extended blockstate for a texture block
-	 * @param world  World
-	 * @param pos    Pos
-	 * @param state  State
-	 * @return  The extended block state
-	 */
-	public static IBlockState writeTextureBlockState(IBlockAccess world, BlockPos pos, IBlockState state) {
-		TileEntity te = world.getTileEntity(pos);
-		if(te != null) {
-			String texture = getTexturePath(te);
-			if(!texture.isEmpty()) {
-				state = ((IExtendedBlockState)state).withProperty(TextureBlockUtil.TEXTURE_PROP, texture);
-			}
-		}
-		return state;
 	}
 
 	/** Any items which have blockColors methods that throw an exception */
@@ -184,7 +168,7 @@ public final class ClientUtil {
 	 * @param index  Tint index
 	 * @return  color, or -1 for undefined
 	 */
-	public static int getStackBlockColorsSafe(ItemStack stack, @Nullable IBlockAccess world, @Nullable BlockPos pos, int index) {
+	public static int getStackBlockColorsSafe(ItemStack stack, @Nullable IEnviromentBlockReader world, @Nullable BlockPos pos, int index) {
 		if(stack.isEmpty()) {
 			return -1;
 		}
@@ -202,7 +186,7 @@ public final class ClientUtil {
 		}
 
 		// fallback to item colors
-		return mc.getItemColors().colorMultiplier(stack, index);
+		return mc.getItemColors().getColor(stack, index);
 	}
 
 	/**
@@ -213,13 +197,13 @@ public final class ClientUtil {
 	 * @param index  Tint index
 	 * @return  color, or -1 for undefined
 	 */
-	public static int getStackBlockColors(ItemStack stack, @Nullable IBlockAccess world, @Nullable BlockPos pos, int index) {
-		if(stack.isEmpty() || !(stack.getItem() instanceof ItemBlock)) {
+	public static int getStackBlockColors(ItemStack stack, @Nullable IEnviromentBlockReader world, @Nullable BlockPos pos, int index) {
+		if(stack.isEmpty() || !(stack.getItem() instanceof BlockItem)) {
 			return -1;
 		}
-		ItemBlock item = (ItemBlock) stack.getItem();
-		IBlockState iblockstate = item.getBlock().getStateFromMeta(item.getMetadata(stack));
-		return mc.getBlockColors().colorMultiplier(iblockstate, world, pos, index);
+		BlockItem item = (BlockItem) stack.getItem();
+		BlockState state = item.getBlock().getDefaultState();
+		return mc.getBlockColors().getColor(state, world, pos, index);
 	}
 
 	/**
@@ -233,8 +217,8 @@ public final class ClientUtil {
 	 */
 	public static void renderJEICauldronFluid(Minecraft mc, int x, int y, ResourceLocation location, float[] color, int level) {
 		GlStateManager.enableBlend();
-		mc.renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		GlStateManager.color(color[0], color[1], color[2]);
+		mc.gameRenderer.enableLightmap();
+		GlStateManager.color3f(color[0], color[1], color[2]);
 		// 0 means JEI ingredient list
 		TextureAtlasSprite sprite = ClientUtil.getSprite(location);
 		if(level == 0) {
@@ -243,7 +227,7 @@ public final class ClientUtil {
 			int height = ((10 * level) / InspirationsRegistry.getCauldronMax());
 			ClientUtil.renderFilledSprite(sprite, x, y, 10, height);
 		}
-		GlStateManager.color(1, 1, 1);
+		GlStateManager.color3f(1, 1, 1);
 		GlStateManager.disableBlend();
 	}
 }
