34e16b575c159610e5276809ec0b78aa5fdb0830
TeamSpen210 <spencerb21@live.com>
2019-08-03 20:35:35 +1000

    Update RecipeUtil for post-Flattening

--- src/main/java/knightminer/inspirations/library/util/RecipeUtil.java
+++ src/main/java/knightminer/inspirations/library/util/RecipeUtil.java
@@ -2,22 +2,22 @@ package knightminer.inspirations.library.util;
 
 import java.util.function.Consumer;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
-import net.minecraft.state.BlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.OreDictionary;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public final class RecipeUtil {
 	private static final Logger log = LogManager.getLogger("inspirations-lib");
@@ -31,8 +31,7 @@ public final class RecipeUtil {
 	 * @return  True if the string is valid to parse with
 	 */
 	public static boolean isValidItemStack(String string, boolean allowWildcard) {
-		String metaString = allowWildcard ? "(-1|[0-9]+)" : "[0-9]+";
-		return string.matches("^[a-z0-9_.-]+:[a-z0-9_.-]+(:" + metaString + ")?$");
+		return string.matches("^[a-z0-9_.-]+:[a-z0-9_.-]+$");
 	}
 
 	/**
@@ -69,91 +68,63 @@ public final class RecipeUtil {
 		if(stack.isEmpty()) {
 			return;
 		}
-
-		// if wildcard, run all stacks
-		if(stack.getMetadata() == OreDictionary.WILDCARD_VALUE) {
-			NonNullList<ItemStack> subItems = NonNullList.create();
-			stack.getItem().getSubItems(CreativeTabs.SEARCH, subItems);
-			for(ItemStack subStack : subItems) {
-				callback.accept(subStack);
-			}
-		} else {
-			callback.accept(stack);
+		NonNullList<ItemStack> subItems = NonNullList.create();
+		stack.getItem().fillItemGroup(ItemGroup.SEARCH, subItems);
+		for(ItemStack subStack : subItems) {
+			callback.accept(subStack);
 		}
 	}
 
 	/**
 	 * Gets a block and meta from a string
 	 * @param string  Input string in the format "modid:block[:meta]"
-	 * @return  Pair of block and meta, or block and null if meta is unspecified or -1. Null if errored
+	 * @return the block, or null if errored
 	 */
-	private static Pair<Block, Integer> getBlockFromString(String string) {
+	@Nullable
+	private static Block getBlockFromString(String string) {
 		if(!isValidItemStack(string, false)) {
 			log.warn("Invalid block string {}", string);
 			return null;
 		}
 
-		String[] parts = string.split(":");
-		Block block = GameRegistry.findRegistry(Block.class).getValue(new ResourceLocation(parts[0], parts[1]));
+		Block block = GameRegistry.findRegistry(Block.class).getValue(new ResourceLocation(string));
 		if(block == null || block == Blocks.AIR) {
 			log.debug("Failed to find block {}", string);
 			return null;
 		}
-
-		Integer meta = null;
-		if(parts.length > 2) {
-			// already validated above
-			meta = Integer.parseInt(parts[2]);
-			if(meta == -1) {
-				meta = null;
-			}
-		}
-
-		return Pair.of(block, meta);
+		return block;
 	}
 
 	/**
 	 * Returns a blockstate from the given string
-	 * @param string  Input string, in the form "modid:block[:meta]". If meta is unset or -1 it returns the default state
+	 * @param string  Input string, in the form "modid:block".
 	 * @return  Block state for the given string
 	 */
-	@SuppressWarnings("deprecation")
-	public static IBlockState getBlockStateFromString(String string) {
-		Pair<Block, Integer> pair = getBlockFromString(string);
-		if(pair == null) {
-			return null;
-		}
-		if(pair.getRight() == null) {
-			return pair.getLeft().getDefaultState();
-		} else {
-			return pair.getLeft().getStateFromMeta(pair.getRight());
-		}
+	@Nullable
+	public static BlockState getBlockStateFromString(String string) {
+		Block block = getBlockFromString(string);
+		return (block == null) ? null : block.getDefaultState();
 	}
 
 	/**
 	 * Runs the given callback on all states returned by the block state string
-	 * @param string         Input string in the format "modid:block[:meta]"
+	 * @param string         Input string in the format "modid:block"
 	 * @param stateConsumer  Callback for block state results
-	 * @param blockConsumer  Callback for block results. If null, runs the state consumer on all possible block states
+	 * @param blockConsumer  Callback for block results. If null, runs the state consumer on all possible block states.
 	 */
-	@SuppressWarnings("deprecation")
-	public static void forBlockInString(String string, Consumer<IBlockState> stateConsumer, Consumer<Block> blockConsumer) {
-		Pair<Block, Integer> pair = getBlockFromString(string);
-		if(pair == null) {
+	public static void forBlockInString(String string, Consumer<BlockState> stateConsumer, Consumer<Block> blockConsumer) {
+		Block block = getBlockFromString(string);
+		if(block == null) {
 			return;
 		}
-		if(pair.getRight() == null) {
-			// if we have no metadata and no block consumer, run the state consumer on all valid states
-			if(blockConsumer == null) {
-				for(IBlockState state : pair.getLeft().getBlockState().getValidStates()) {
-					stateConsumer.accept(state);
-				}
-			} else {
-				// if there is a block consumer use that
-				blockConsumer.accept(pair.getLeft());
+		// if we have no block consumer, run the state consumer on all valid states
+		if(blockConsumer == null) {
+			for(BlockState state : block.getStateContainer().getValidStates()) {
+				stateConsumer.accept(state);
 			}
 		} else {
-			stateConsumer.accept(pair.getLeft().getStateFromMeta(pair.getRight()));
+			// if there is a block consumer use that
+			blockConsumer.accept(block);
 		}
 	}
 }
