95c3ff31d68e21848c1a54855f481e2ab8edc570
TeamSpen210 <spencerb21@live.com>
2019-08-06 18:19:35 +1000

    Reimplement Waypoint Compass tinting handler

--- src/main/java/knightminer/inspirations/tools/ToolsClientProxy.java
+++ src/main/java/knightminer/inspirations/tools/ToolsClientProxy.java
@@ -2,46 +2,24 @@ package knightminer.inspirations.tools;
 
 import knightminer.inspirations.common.ClientProxy;
 import knightminer.inspirations.tools.client.RenderModArrow;
-import knightminer.inspirations.tools.entity.EntityModArrow;
-import knightminer.inspirations.tools.item.ItemModArrow.ArrowType;
+import knightminer.inspirations.tools.entity.RedstoneArrow;
 import knightminer.inspirations.tools.item.ItemWaypointCompass;
-import knightminer.inspirations.utility.block.BlockRedstoneCharge;
-import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.client.renderer.color.ItemColors;
-import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.DyeColor;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.Map;
 
 public class ToolsClientProxy extends ClientProxy {
 
-	@Override
-	public void preInit() {
-		super.preInit();
-		RenderingRegistry.registerEntityRenderingHandler(EntityModArrow.class, RenderModArrow::new);
-	}
-
 	@SubscribeEvent
-	public void registerModels(ModelRegistryEvent event) {
-		setModelStateMapper(InspirationsTools.redstoneCharge, new StateMap.Builder().ignore(BlockRedstoneCharge.FACING, BlockRedstoneCharge.QUICK).build());
-
-		// items
-		registerItemModel(InspirationsTools.redstoneCharger);
-		registerItemModel(InspirationsTools.woodenCrook);
-		registerItemModel(InspirationsTools.stoneCrook);
-		registerItemModel(InspirationsTools.boneCrook);
-		registerItemModel(InspirationsTools.blazeCrook);
-		registerItemModel(InspirationsTools.witherCrook);
-		registerItemModel(InspirationsTools.northCompass);
-		registerItemModel(InspirationsTools.barometer);
-		registerItemModel(InspirationsTools.photometer);
-		registerItemModel(InspirationsTools.waypointCompass);
-
-		// items
-		for(ArrowType type : ArrowType.values()) {
-			registerItemModel(InspirationsTools.arrow, type.getMeta(), type.getName());
-		}
+	public void clientSetup(FMLClientSetupEvent event) {
+		super.preInit();
+		RenderingRegistry.registerEntityRenderingHandler(RedstoneArrow.class, RenderModArrow::new);
 	}
 
 	@SubscribeEvent
@@ -49,20 +27,29 @@ public class ToolsClientProxy extends ClientProxy {
 		ItemColors itemColors = event.getItemColors();
 
 		// dyed compass
-		registerItemColors(itemColors, (stack, tintIndex) -> {
-			EnumDyeColor color = EnumDyeColor.byMetadata(stack.getMetadata());
-			if(tintIndex == 0) {
-				if (color == EnumDyeColor.BLACK) {
-					return 0x444444;
-				} else if (color == EnumDyeColor.WHITE) {
-					return 0xDDDDDD;
-				}
-				return color.colorValue;
-			}
-			if(tintIndex == 1) {
-				return ItemWaypointCompass.getNeedleColor(color);
-			}
-			return -1;
-		}, InspirationsTools.waypointCompass);
+        for (Map.Entry<DyeColor, ItemWaypointCompass> entry: InspirationsTools.waypointCompasses.entrySet()) {
+            ItemWaypointCompass compass = entry.getValue();
+            DyeColor color = compass.getColor();
+
+            int needleColor = ItemWaypointCompass.getNeedleColor(color);
+            int bodyColor;
+            if (color == DyeColor.BLACK) {
+                bodyColor = 0x444444;
+            } else if (color == DyeColor.WHITE) {
+                bodyColor = 0xDDDDDD;
+            } else {
+                bodyColor = color.colorValue;
+            }
+            itemColors.register((stack, tintIndex) -> {
+                switch (tintIndex) {
+                    case 0:
+                        return bodyColor;
+                    case 1:
+                        return needleColor;
+                    default:
+                        return -1;
+                }
+            }, compass);
+        }
 	}
 }
