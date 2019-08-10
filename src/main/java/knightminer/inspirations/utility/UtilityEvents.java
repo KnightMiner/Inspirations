3618e4bc3e870a587e430c4e4d693672c3b25643
TeamSpen210 <spencerb21@live.com>
2019-08-10 16:32:22 +1000

    Update logic for placing carpets on pressure plates

--- src/main/java/knightminer/inspirations/utility/UtilityEvents.java
+++ src/main/java/knightminer/inspirations/utility/UtilityEvents.java
@@ -1,42 +1,39 @@
 package knightminer.inspirations.utility;
 
 import knightminer.inspirations.common.Config;
-import knightminer.inspirations.utility.block.BlockCarpetedPressurePlate;
 import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.block.BlockHopper;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.EnumDyeColor;
-import net.minecraft.item.Item;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
-import net.minecraftforge.fml.common.eventhandler.Event.Result;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class UtilityEvents {
 	@SubscribeEvent
 	public static void placeCarpetOnPressurePlate(RightClickBlock event) {
-		if(!Config.enableCarpetedPressurePlate) {
+		if(!Config.enableCarpetedPressurePlate.get()) {
 			return;
 		}
 
 		// must be using carpet
 		ItemStack stack = event.getItemStack();
-		if(stack.getItem() != Item.getItemFromBlock(Blocks.CARPET)) {
+		Block carpetBlock = Block.getBlockFromItem(stack.getItem());
+		if(!(carpetBlock instanceof CarpetBlock)) {
 			return;
 		}
 
 		// must be clicking a stone pressure plate or the block below one
 		World world = event.getWorld();
 		BlockPos pos = event.getPos();
-		IBlockState current = world.getBlockState(pos);
+		BlockState current = world.getBlockState(pos);
 		if(current.getBlock() != Blocks.STONE_PRESSURE_PLATE) {
 			pos = pos.up();
 			current = world.getBlockState(pos);
@@ -46,19 +43,13 @@ public class UtilityEvents {
 		}
 
 		// determine the state to place
-		IBlockState state;
-		int meta = stack.getMetadata();
-		EnumDyeColor color = EnumDyeColor.byMetadata(meta);
-		if(meta < 8) {
-			state = InspirationsUtility.carpetedPressurePlate1.getDefaultState()
-					.withProperty(BlockCarpetedPressurePlate.COLOR1, color);
-		} else {
-			state = InspirationsUtility.carpetedPressurePlate2.getDefaultState()
-					.withProperty(BlockCarpetedPressurePlate.COLOR2, color);
-		}
+		DyeColor color = ((CarpetBlock)carpetBlock).getColor();
+
+		BlockState state = InspirationsUtility.carpetedPressurePlates[color.getId()].getDefaultState();
+		state = state.updatePostPlacement(Direction.DOWN, world.getBlockState(pos.down()), world, pos, pos.down());
 
 		// play sound
-		EntityPlayer player = event.getEntityPlayer();
+		PlayerEntity player = event.getEntityPlayer();
 		SoundType sound = state.getBlock().getSoundType(state, world, pos, player);
 		world.playSound(player, pos, sound.getPlaceSound(), SoundCategory.BLOCKS, (sound.getVolume() + 1.0F) / 2.0F, sound.getPitch() * 0.8F);
 
@@ -68,17 +59,17 @@ public class UtilityEvents {
 			world.setBlockState(pos, state);
 
 			// add statistic
-			if (player instanceof EntityPlayerMP) {
-				CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, stack);
+			if (player instanceof ServerPlayerEntity) {
+				CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayerEntity)player, pos, stack);
 			}
 
 			// take one carpet
-			if(!player.capabilities.isCreativeMode) {
+			if(!player.isCreative()) {
 				stack.shrink(1);
 			}
 		}
 		event.setCanceled(true);
-		event.setCancellationResult(EnumActionResult.SUCCESS);
+		event.setCancellationResult(ActionResultType.SUCCESS);
 	}
 
 	/**
@@ -86,15 +77,15 @@ public class UtilityEvents {
 	 */
 	@SubscribeEvent
 	public static void clickHopperWithPipe(RightClickBlock event) {
-		if(!Config.enablePipe || event.getItemStack().getItem() != InspirationsUtility.pipeItem) {
+		if(!Config.enablePipe.get() || event.getItemStack().getItem() != InspirationsUtility.pipeItem) {
 			return;
 		}
 		World world = event.getWorld();
-		if(world.isRemote || !(world.getBlockState(event.getPos()).getBlock() instanceof BlockHopper)) {
+		if(world.isRemote || !(world.getBlockState(event.getPos()).getBlock() instanceof HopperBlock)) {
 			return;
 		}
 
-		event.setUseBlock(Result.DENY);
-		event.setUseItem(Result.ALLOW);
+		event.setUseBlock(Event.Result.DENY);
+		event.setUseItem(Event.Result.ALLOW);
 	}
 }
