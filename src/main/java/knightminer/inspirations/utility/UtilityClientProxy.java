35d128c3b4cabc02954056023f8c00b7732ae8f5
TeamSpen210 <spencerb21@live.com>
2019-08-10 18:59:56 +1000

    Register Collector and Pipe GUIs

--- src/main/java/knightminer/inspirations/utility/UtilityClientProxy.java
+++ src/main/java/knightminer/inspirations/utility/UtilityClientProxy.java
@@ -2,14 +2,25 @@ package knightminer.inspirations.utility;
 
 import knightminer.inspirations.common.ClientProxy;
 import knightminer.inspirations.utility.block.BlockRedstoneBarrel;
+import knightminer.inspirations.utility.client.CollectorScreen;
+import knightminer.inspirations.utility.client.PipeScreen;
 import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class UtilityClientProxy extends ClientProxy {
 
+	@SubscribeEvent
+	public void commonSetup(FMLCommonSetupEvent event) {
+
+		// Register GUIs.
+		ScreenManager.registerFactory(InspirationsUtility.contCollector, CollectorScreen::new);
+		ScreenManager.registerFactory(InspirationsUtility.contPipe, PipeScreen::new);
+	}
 
 	@SubscribeEvent
 	public void registerBlockColors(ColorHandlerEvent.Block event) {
