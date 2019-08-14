5977b20338622451635de66b313b2f67a0d4e33b
TeamSpen210 <spencerb21@live.com>
2019-08-14 10:35:53 +1000

    Convert over Enchantable Shield class

--- src/main/java/knightminer/inspirations/tools/item/ItemEnchantableShield.java
+++ src/main/java/knightminer/inspirations/tools/item/ItemEnchantableShield.java
@@ -1,17 +1,18 @@
 package knightminer.inspirations.tools.item;
 
-import net.minecraft.item.ItemShield;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
 
-public class ItemEnchantableShield extends ItemShield {
-  public ItemEnchantableShield() {
-    super();
-    this.setUnlocalizedName("shield");
+public class ItemEnchantableShield extends ShieldItem {
+  public ItemEnchantableShield(Item.Properties props) {
+    super(props);
+    this.setRegistryName("shield");
   }
 
   @Override
   public int getItemEnchantability(ItemStack stack) {
     // small boost to enchantability if it has a banner
-    return stack.getSubCompound("BlockEntityTag") != null ? 16 : 12;
+    return stack.getChildTag("BlockEntityTag") != null ? 16 : 12;
   }
 }
