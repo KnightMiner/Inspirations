e0b3648244843fc237fab938d021e7cb81bb936c
TeamSpen210 <spencerb21@live.com>
2019-12-30 17:45:06 +1000

    Use javadoc format for tag comments

--- src/main/java/knightminer/inspirations/library/InspirationsTags.java
+++ src/main/java/knightminer/inspirations/library/InspirationsTags.java
@@ -7,7 +7,6 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 
 public class InspirationsTags {
-
 	public static class Blocks {
 		public static final Tag<Block> MULCH = new BlockTags.Wrapper(Util.getResource("mulch"));
 		public static final Tag<Block> SMALL_FLOWERS = new BlockTags.Wrapper(Util.getResource("small_flowers"));
@@ -17,30 +16,30 @@ public class InspirationsTags {
 		public static final Tag<Block> BOOKSHELVES = new BlockTags.Wrapper(Util.getResource("bookshelves"));
 		public static final Tag<Block> ENLIGHTENED_BUSHES = new BlockTags.Wrapper(Util.getResource("enlightened_bushes"));
 
-		// Blocks with this tag act as fire for the cauldron.
+		/** Blocks with this tag act as fire for the cauldron. */
 		public static final Tag<Block> CAULDRON_FIRE = new BlockTags.Wrapper(Util.getResource("cauldron_fire"));
 	}
 
 	public static class Items {
-		// Duplicates of above.
+		// Duplicates of above. Flower pots and pressure plates have no item form.
 		public static final Tag<Item> MULCH = new ItemTags.Wrapper(Util.getResource("mulch"));
 		public static final Tag<Item> SMALL_FLOWERS = new ItemTags.Wrapper(Util.getResource("small_flowers"));
 		public static final Tag<Item> CARPETED_TRAPDOORS = new ItemTags.Wrapper(Util.getResource("carpeted_trapdoors"));
 		public static final Tag<Item> BOOKSHELVES = new ItemTags.Wrapper(Util.getResource("bookshelves"));
 		public static final Tag<Item> ENLIGHTENED_BUSHES = new ItemTags.Wrapper(Util.getResource("enlightened_bushes"));
 
-		// Items with this tag are registered to perform cauldron recipes.
+		/** Items with this tag are registered to perform cauldron recipes. */
 		public static final Tag<Item> DISP_CAULDRON_RECIPES = new ItemTags.Wrapper(Util.getResource("cauldron_recipes"));
 
-		// Items with this tag are registered to have fluid tank functionality.
+		/** Items with this tag are registered to have fluid tank functionality. */
 		public static final Tag<Item> DISP_FLUID_TANKS = new ItemTags.Wrapper(Util.getResource( "fluid_containers"));
 
 		public static final Tag<Item> MILK_CONTAINERS = new ItemTags.Wrapper(Util.getResource("milk_containers"));
 
-		// Items which are valid to be placed on bookshelves.
+		/** Items which are valid to be placed on bookshelves. */
 		public static final Tag<Item> BOOKS = new ItemTags.Wrapper(Util.getResource("books"));
 
-		// Vanilla carpets, for recipe use.
+		/** Vanilla carpets, for recipe use. */
 		public static final Tag<Item> CARPETS = new ItemTags.Wrapper(Util.getResource("carpets"));
 
 		public static final Tag<Item> WAYPOINT_COMPASSES = new ItemTags.Wrapper(Util.getResource("waypoint_compasses"));
