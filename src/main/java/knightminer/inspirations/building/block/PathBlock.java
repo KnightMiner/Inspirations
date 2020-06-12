70ed082fc4bc7c9619c7bd1d58797f4b3614385d
TeamSpen210 <spencerb21@live.com>
2020-06-12 17:06:37 +1000

    Make paths waterloggable.

--- src/main/java/knightminer/inspirations/building/block/PathBlock.java
+++ src/main/java/knightminer/inspirations/building/block/PathBlock.java
@@ -4,24 +4,33 @@ import knightminer.inspirations.common.Config;
 import knightminer.inspirations.common.block.HidableBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nonnull;
 
-public class PathBlock extends HidableBlock {
-
+public class PathBlock extends HidableBlock implements IWaterLoggable {
 	private final VoxelShape shape;
 	private final VoxelShape collShape;
+
+	public static BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
 	public PathBlock(VoxelShape shape, MaterialColor mapColor) {
 		super(Block.Properties.create(Material.ROCK, mapColor)
@@ -32,6 +41,30 @@ public class PathBlock extends HidableBlock {
 		// Each path has a different shape, but use the bounding box for collisions.
 		this.shape = shape;
 		this.collShape = VoxelShapes.create(shape.getBoundingBox());
+		setDefaultState(getDefaultState().with(WATERLOGGED, false));
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(WATERLOGGED);
+	}
+
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		IFluidState fluid = context.getWorld().getFluidState(context.getPos());
+		return getDefaultState().with(WATERLOGGED, fluid.getFluid() == Fluids.WATER);
+	}
+
+	@Nonnull
+	public IFluidState getFluidState(BlockState state) {
+	  return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+
+	@Nonnull
+	public BlockState updatePostPlacement(BlockState state, @Nonnull Direction facing, @Nonnull BlockState facingState, @Nonnull IWorld world, @Nonnull BlockPos currentPos, @Nonnull BlockPos facingPos) {
+		if (state.get(WATERLOGGED)) {
+			world.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.updatePostPlacement(state, facing, facingState, world, currentPos, facingPos);
 	}
 
 	/* Block Shape */
@@ -66,14 +99,14 @@ public class PathBlock extends HidableBlock {
 	@Deprecated
 	@Nonnull
 	@Override
-	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+	public VoxelShape getShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
 		return shape;
 	}
 
 	@Deprecated
 	@Nonnull
 	@Override
-	public VoxelShape getCollisionShape(@Nonnull BlockState state, @Nonnull IBlockReader world, @Nonnull BlockPos pos, ISelectionContext context) {
+	public VoxelShape getCollisionShape(@Nonnull BlockState state, @Nonnull IBlockReader world, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
 		return collShape;
 	}
 
@@ -81,7 +114,7 @@ public class PathBlock extends HidableBlock {
 
 	@Deprecated
 	@Override
-	public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos pos) {
+	public boolean isValidPosition(@Nonnull BlockState state, @Nonnull IWorldReader world, @Nonnull BlockPos pos) {
 		return super.isValidPosition(state, world, pos) && this.canBlockStay(world, pos);
 	}
 
@@ -93,9 +126,11 @@ public class PathBlock extends HidableBlock {
 
 	@Deprecated
 	@Override
-	public void neighborChanged(BlockState state, World world, BlockPos pos, Block blockIn, BlockPos fromPos, boolean p_220069_6_) {
+	public void neighborChanged(@Nonnull BlockState state, @Nonnull World world, @Nonnull BlockPos pos, @Nonnull Block other, @Nonnull BlockPos fromPos, boolean isMoving) {
 		if (!this.canBlockStay(world, pos)) {
 			world.destroyBlock(pos, true);
+		} else if (state.get(WATERLOGGED)) {
+			world.getPendingFluidTicks().scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
 		}
 	}
 }
